7754ee5ec3c2ff152b537eab0351e309b8e8bd2a
Ken Johnson <dragonkenkj@gmail.com>
2021-01-12 15:58:05 -0500

    Added simple chart

--- tennis_pred_app.py
+++ tennis_pred_app.py
@@ -1,5 +1,7 @@
-import json
 import streamlit as st
+import pandas as pd
+import numpy as np
+import json
 import os
 
 from tensorflow import keras
@@ -7,6 +9,7 @@ from tensorflow import keras
 
 def main():
     player_id, tourn_id = load_ids()
+    avg_ages, years = winner_age()
     round_id = {
         0: 0,
         "RR": 1,
@@ -32,21 +35,21 @@ def main():
     col1.header("Player 1")
     col2.header("Player 2")
     p1_name = col1.multiselect("Name", options=list(player_id.keys()), key=0)
-    p1_seed = int(col1.text_input("Seed", value=0, key=1))
+    p1_seed_str = col1.text_input("Seed", value=0, key=1)
     p1_entry = col1.multiselect(
         "Entry", options=["R", "PR", "LL", "WC", "Q", "S", "SE"], key=2
     )
-    p1_rank = int(col1.text_input("Rank", value=0, key=3))
-    p1_rank_pts = int(col1.text_input("Rank Points", value=0, key=4))
-    p1_age = int(col1.text_input("Age", value=0, key=5))
+    p1_rank_str = col1.text_input("Rank", value=0, key=3)
+    p1_rank_pts_str = col1.text_input("Rank Points", value=0, key=4)
+    p1_age_str = col1.text_input("Age", value=0, key=5)
     p2_name = col2.multiselect("Name", options=list(player_id.keys()), key=6)
-    p2_seed = int(col2.text_input("Seed", value=0, key=7))
+    p2_seed_str = col2.text_input("Seed", value=0, key=7)
     p2_entry = col2.multiselect(
         "Entry", options=["R", "PR", "LL", "WC", "Q", "S", "SE"], key=8
     )
-    p2_rank = int(col2.text_input("Rank", value=0, key=9))
-    p2_rank_pts = int(col2.text_input("Rank Points", value=0, key=10))
-    p2_age = int(col2.text_input("Age", value=0, key=11))
+    p2_rank_str = col2.text_input("Rank", value=0, key=9)
+    p2_rank_pts_str = col2.text_input("Rank Points", value=0, key=10)
+    p2_age_str = col2.text_input("Age", value=0, key=11)
 
     if st.button("Predict"):
         t_id, t_surface_id, t_size, t_level_id, t_best_of = tourn_id[t_name[0]]
@@ -55,9 +58,15 @@ def main():
         t_round_id = round_id[t_round[0]]
         p1_entry_id = entry_id[p1_entry[0]]
         p2_entry_id = entry_id[p2_entry[0]]
-        p1_seed = 0 if p1_seed == "N/A" else p1_seed
-        p2_seed = 0 if p2_seed == "N/A" else p2_seed
-        data = [
+        p1_seed = 0 if p1_seed_str == "N/A" else int(p1_seed_str)
+        p2_seed = 0 if p2_seed_str == "N/A" else int(p2_seed_str)
+        p1_rank = int(p1_rank_str)
+        p2_rank = int(p2_rank_str)
+        p1_rank_pts = int(p1_rank_pts_str)
+        p2_rank_pts = int(p2_rank_pts_str)
+        p1_age = int(p1_age_str)
+        p2_age = int(p2_age_str)
+        data1 = [
             [
                 t_id,
                 t_surface_id,
@@ -73,7 +82,7 @@ def main():
                 p1_ioc_id,
                 p1_age,
                 p1_rank,
-                p1_rank_pts,
+                # p1_rank_pts,
                 p2_id,
                 p2_seed,
                 p2_entry_id,
@@ -82,19 +91,65 @@ def main():
                 p2_ioc_id,
                 p2_age,
                 p2_rank,
-                p2_rank_pts,
+                # p2_rank_pts,
             ]
         ]
-        print(data)
+        data2 = [
+            [
+                t_id,
+                t_surface_id,
+                t_size,
+                t_level_id,
+                t_best_of,
+                t_round_id,
+                p2_id,
+                p2_seed,
+                p2_entry_id,
+                p2_hand_id,
+                p2_ht,
+                p2_ioc_id,
+                p2_age,
+                p2_rank,
+                # p2_rank_pts,
+                p1_id,
+                p1_seed,
+                p1_entry_id,
+                p1_hand_id,
+                p1_ht,
+                p1_ioc_id,
+                p1_age,
+                p1_rank,
+                # p1_rank_pts,
+            ]
+        ]
         model = keras.models.load_model("models")
-        prediction = model.predict(data)
-        print(prediction)
-        st.write(
-            "Predicted winner is: {}".format(
-                [p1_name[0], p2_name[0]][int(prediction[0][0])]
-            )
-        )
-        print(prediction)
+        pred1 = model.predict(data1)
+        pred2 = model.predict(data2)
+        prediction = np.asarray([(pred1[0][i] + pred2[0][1 - i]) / 2 for i in range(2)])
+        players = [p1_name[0], p2_name[0]]
+        winner = players[prediction.argmax()]
+        probability = np.amax(prediction)
+        st.write("Predicted winner is: {} ({}%)".format(winner, probability * 100))
+
+    st.line_chart({"Average Age": avg_ages})
+
+
+def winner_age():
+    avg_ages = []
+    headers = list(pd.read_csv("clean_data/atp_matches_2000.csv"))
+    for fn_csv in sorted(os.listdir("clean_data")):
+        fp_csv = os.path.join("clean_data", fn_csv)
+        df = pd.read_csv(fp_csv, usecols=["winner_age", "round"])
+        if fn_csv == "atp_matches_2016.csv" or fn_csv == "atp_matches_2017.csv":
+            df = pd.read_csv(fp_csv, names=headers)
+        df = df.dropna(subset=["winner_age", "round"])
+        df = df[df["round"].map(lambda x: x == "F")]
+        ages = df["winner_age"].values
+        if fn_csv == "atp_matches_2016.csv" or fn_csv == "atp_matches_2017.csv":
+            ages = [(float(str(age))) for age in ages]
+        avg_age = sum(ages) / len(ages)
+        avg_ages.append(avg_age)
+    return avg_ages, range(2000, 2018)
 
 
 def load_ids():
--- tennis_pred_train.py
+++ tennis_pred_train.py
@@ -18,13 +18,13 @@ ID_PATH = "ids"
 def get_model():
     model = Sequential(
         [
-            Dense(64, input_dim=24, activation="relu"),
-            Dropout(0),
+            Dense(64, input_dim=22, activation="relu"),
+            Dropout(0.2),
             Dense(64, activation="relu"),
-            Dropout(0),
+            Dropout(0.2),
             Dense(64, activation="relu"),
-            Dropout(0),
-            Dense(1, activation="sigmoid"),
+            Dropout(0.2),
+            Dense(2, activation="softmax"),
         ]
     )
     model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
@@ -77,17 +77,12 @@ def get_data():
         "best_of",
         "round",
     ]
+    na_ok_cols = set(["winner_seed", "winner_entry", "loser_seed", "loser_entry"])
     for fn_csv in os.listdir("clean_data"):
-        df = pd.read_csv(
-            os.path.join("clean_data", fn_csv),
-            usecols=headers,
-        )
-        df = df.dropna(
-            subset=list(
-                set(headers)
-                - set(["winner_seed", "winner_entry", "loser_seed", "loser_entry"])
-            )
-        )
+        fp_csv = os.path.join("clean_data", fn_csv)  # File path
+        # Preprocess data
+        df = pd.read_csv(fp_csv, usecols=headers)
+        df = df.dropna(subset=list(set(headers) - na_ok_cols))
         df = df.fillna(0)
         df = df.reset_index()
         len_data = len(df["winner_name"])
@@ -96,89 +91,47 @@ def get_data():
         labels = []  # 0: Player 0, 1: Player 1
         for i in range(len_data):
             # Get data from dataframe
-            t_name, t_surface, t_size, t_level, t_best_of, t_round = [
-                df[x][i] for x in headers[:4] + headers[-2:]
-            ]
-            wl_start = ["winner_", "loser_"]
-            w_name = df["winner_name"][i]
-            l_name = df["loser_name"][i]
-            # Get Player 1 data
-            (
-                p1_seed,
-                p1_entry,
-                p1_name,
-                p1_hand,
-                p1_ht,
-                p1_ioc,
-                p1_age,
-                p1_rank,
-                p1_rank_pts,
-            ) = [
-                df[x][i]
-                for x in headers
-                if x.startswith(wl_start[not (w_name < l_name)])
-            ]
-            # Get Player 2 data
-            (
-                p2_seed,
-                p2_entry,
-                p2_name,
-                p2_hand,
-                p2_ht,
-                p2_ioc,
-                p2_age,
-                p2_rank,
-                p2_rank_pts,
-            ) = [df[x][i] for x in headers if x.startswith(wl_start[w_name < l_name])]
-            labels.append(int(not (w_name < l_name)))
+            c_data = (
+                t_name,
+                t_surface,
+                t_size,
+                t_level,
+                w_seed,
+                w_entry,
+                w_name,
+                w_hand,
+                w_ht,
+                w_ioc,
+                w_age,
+                w_rank,
+                w_rank_pts,
+                l_seed,
+                l_entry,
+                l_name,
+                l_hand,
+                l_ht,
+                l_ioc,
+                l_age,
+                l_rank,
+                l_rank_pts,
+                t_best_of,
+                t_round,
+            ) = df.iloc[[i]].values[0][1:]
             # Convert string data to ID's
             t_surface_id = surface_id[t_surface]
             t_level_id = level_id[t_level]
             t_round_id = round_id[t_round]
-            p1_entry_id = entry_id[p1_entry]
-            p2_entry_id = entry_id[p2_entry]
-            p1_hand_id = hand_id[p1_hand]
-            p2_hand_id = hand_id[p2_hand]
-            if t_name in tourn_id.keys():  # Set tourney ID
-                t_id = tourn_id[t_name][0]
-            else:
-                t_id = len(tourn_id)
-                tourn_id[t_name] = list(
-                    map(
-                        int,
-                        [
-                            t_id,
-                            t_surface_id,
-                            t_size,
-                            t_level_id,
-                            t_best_of,
-                        ],
-                    )
-                )
-            if p1_ioc in ioc_id.keys():  # Set P1 IOC ID
-                p1_ioc_id = ioc_id[p1_ioc][0]
-            else:
-                p1_ioc_id = len(ioc_id)
-                ioc_id[p1_ioc] = [p1_ioc_id]
-            if p2_ioc in ioc_id.keys():  # Set P2 IOC ID
-                p2_ioc_id = ioc_id[p2_ioc][0]
-            else:
-                p2_ioc_id = len(ioc_id)
-                ioc_id[p2_ioc] = [p2_ioc_id]
-            if p1_name in player_id.keys():  # Set P1 ID
-                p1_id = player_id[p1_name][0]
-            else:
-                p1_id = len(player_id)
-                player_id[p1_name] = list(
-                    map(int, [p1_id, p1_hand_id, p1_ht, p1_ioc_id])
-                )
-            if p2_name in player_id.keys():  # Set P2 ID
-                p2_id = player_id[p2_name][0]
-            else:
-                p2_id = len(player_id)
-                player_id[p2_name] = list(
-                    map(int, [p2_id, p2_hand_id, p2_ht, p2_ioc_id])
-                )
+            w_entry_id = entry_id[w_entry]
+            l_entry_id = entry_id[l_entry]
+            w_hand_id = hand_id[w_hand]
+            l_hand_id = hand_id[l_hand]
+            t_id = set_id(
+                t_name, tourn_id, [t_surface_id, t_size, t_level_id, t_best_of]
+            )
+            w_ioc_id = set_id(w_ioc, ioc_id)
+            l_ioc_id = set_id(l_ioc, ioc_id)
+            w_id = set_id(w_name, player_id, [w_hand_id, w_ht, w_ioc_id])
+            l_id = set_id(l_name, player_id, [l_hand_id, l_ht, l_ioc_id])
             data.append(
                 [
                     t_id,
@@ -187,31 +140,71 @@ def get_data():
                     t_level_id,
                     t_best_of,
                     t_round_id,
-                    p1_id,
-                    p1_seed,
-                    p1_entry_id,
-                    p1_hand_id,
-                    p1_ht,
-                    p1_ioc_id,
-                    p1_age,
-                    p1_rank,
-                    p1_rank_pts,
-                    p2_id,
-                    p2_seed,
-                    p2_entry_id,
-                    p2_hand_id,
-                    p2_ht,
-                    p2_ioc_id,
-                    p2_age,
-                    p2_rank,
-                    p2_rank_pts,
+                    w_id,
+                    w_seed,
+                    w_entry_id,
+                    w_hand_id,
+                    w_ht,
+                    w_ioc_id,
+                    w_age,
+                    w_rank,
+                    # w_rank_pts,
+                    l_id,
+                    l_seed,
+                    l_entry_id,
+                    l_hand_id,
+                    l_ht,
+                    l_ioc_id,
+                    l_age,
+                    l_rank,
+                    # l_rank_pts,
                 ]
             )
+            labels.append([1, 0])
+            # Reverse winners and losers so model doesn't overfit
+            data.append(
+                [
+                    t_id,
+                    t_surface_id,
+                    t_size,
+                    t_level_id,
+                    t_best_of,
+                    t_round_id,
+                    l_id,
+                    l_seed,
+                    l_entry_id,
+                    l_hand_id,
+                    l_ht,
+                    l_ioc_id,
+                    l_age,
+                    l_rank,
+                    # l_rank_pts,
+                    w_id,
+                    w_seed,
+                    w_entry_id,
+                    w_hand_id,
+                    w_ht,
+                    w_ioc_id,
+                    w_age,
+                    w_rank,
+                    # w_rank_pts,
+                ]
+            )
+            labels.append([0, 1])
     dump_json(
         [tourn_id, player_id],
         ["tourn_id.json", "player_id.json"],
     )
     return np.asarray(data), np.asarray(labels)
+
+
+def set_id(str_key, id_dict, relevant_details=[]):
+    if str_key in id_dict.keys():
+        id_num = id_dict[str_key][0]
+    else:
+        id_num = len(id_dict)
+        id_dict[str_key] = list(map(int, [id_num] + relevant_details))
+    return id_num
 
 
 def dump_json(id_dict, json_names):
